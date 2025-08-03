d818bb2402b791545c9e4ee587efc9a9452903cc
rokish7439 <rokish44@gmail.com>
2025-08-03 21:49:51 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~srlyCSjQ#3KpEP8LqZpOXYakZ8ASHHbbXeIWvXqUVOJH2_8_FzSE ",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  PREFIX: "x." // prefix (e.g., ., /, !, *)
 };
